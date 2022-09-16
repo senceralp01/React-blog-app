3c2933c5205fd4da736a3bbf2894c111e67b2430
sencer2022 <senceralpdogdu@gmail.com>
2022-09-16 20:30:03 +0300

    started to redux

--- src/index.js
+++ src/index.js
@@ -2,7 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import AppRouter from './routers/AppRouter';
-import './App.css'
+import './App.css';
+
+import { createStore } from 'redux';
+
+const initialState = {
+    count: 10
+}
+
+//store
+const store = createStore(( state = initialState ) => {
+    return state
+});
+
+//get store
+console.log(store.getState());
+
+// INCREMENT, DECREMENT, RESET => ACTIONS (Object)
+
+// DISPATCH
+
+// REDUCERS
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( <AppRouter /> );
