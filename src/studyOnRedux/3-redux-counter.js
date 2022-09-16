48c737b4cf68d1134bacfd439479edcedcef28e5
sencer2022 <senceralpdogdu@gmail.com>
2022-09-16 21:35:49 +0300

    studied on redux reducers

--- src/studyOnRedux/3-redux-counter.js
+++ src/studyOnRedux/3-redux-counter.js
@@ -6,7 +6,60 @@ import './App.css';
 
 import { createStore } from 'redux';
 
+const initialState = {
+    count: 0
+}
 
+// REDUCERS
+// Reducer'ı ayrı bir yerde tanımlamamızın sebebi store içerisine farklı reducer'lar göndermek. Örneğin:
+// BLOK reducer
+// LOGIN reducer (auth => username, isLogged)
+const counterReducer = ( state = initialState, action ) => {
+    switch (action.type){
+        case "INCREMENT":
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
+            return {
+                count: state.count + incrementBy
+            }
+        case "DECREMENT":
+            return {
+                count: state.count - 1
+            }
+        case "RESET":
+            return {
+                count: 0
+            }
+        default:
+            return state
+    }        
+}
+
+//store
+const store = createStore(counterReducer);
+
+//get store
+// console.log(store.getState());
+
+store.subscribe(() => { // Her defasında değil yalnızca state üzerinde bir değişiklik olduğunda çalışır.
+    console.log(store.getState());
+})
+
+// INCREMENT, DECREMENT, RESET => ACTIONS (Object)
+store.dispatch({
+    type: "INCREMENT",
+    incrementBy: 10
+});
+
+store.dispatch({
+    type: "INCREMENT"
+});
+
+store.dispatch({
+    type: "RESET"
+});
+
+
+// DISPATCH
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
